b96a0c74a6f1ca5c4f2ea960c7e3ba8fb4a52245
jfcarocota <jfcarocota@gmail.com>
2019-02-16 23:01:17 -0700

    comit 1

---
+++ Assets/Medieval house/Scripts/CinematicCtroller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+
+public class CinematicCtroller : MonoBehaviour
+{
+    PlayableDirector director;
+
+    [SerializeField]
+    Button playButton;
+    [SerializeField]
+    Button pauseButton;
+    [SerializeField]
+    Button stopButton;
+    [SerializeField]
+    Button skipRight;
+    [SerializeField]
+    Button skipLeft;
+
+    void Awake()
+    {
+        director = GetComponent<PlayableDirector>();
+
+        playButton.onClick.AddListener(PlayCinematic);
+        pauseButton.onClick.AddListener(PauseCinematic);
+        stopButton.onClick.AddListener(StopButton);
+    }
+
+    void PlayCinematic()
+    {
+        director.Play();
+    }
+
+    void PauseCinematic()
+    {
+        director.Pause();
+    }
+
+    void StopButton()
+    {
+        director.Stop();
+    }
+
+    void SkipRightButton()
+    {
+        director.time += 20f;
+    }
+
+    void SkipLeftButton()
+    {
+
+    }
+}
